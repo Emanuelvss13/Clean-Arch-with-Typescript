3e4f5426195a078f80fcf64f99bb67d891476ec2
Emanuelvss13 <emanuelsouza2002@gmail.com>
2023-01-25 17:16:54 -0300

    feat: notification pattern

---
+++ src/domain/@shared/notification/notification.spec.ts
@@ -0,0 +1,75 @@
+import { Notification } from "./notification";
+
+describe("Notification unit tests", () => {
+  it("should create errors", () => {
+    const notification = new Notification();
+
+    const error = {
+      message: "error message",
+      context: "customer",
+    };
+
+    notification.addError(error);
+
+    console.log(notification.messages("customer"));
+
+    expect(notification.messages("customer")).toBe("error message");
+
+    const error2 = {
+      message: "error message2",
+      context: "customer",
+    };
+
+    notification.addError(error2);
+
+    expect(notification.messages("customer")).toBe(
+      "error message, error message2"
+    );
+
+    const error3 = {
+      message: "error message2",
+      context: "order",
+    };
+
+    notification.addError(error3);
+
+    expect(notification.messages("customer")).toBe(
+      "error message, error message2"
+    );
+  });
+
+  it("should return 'no errors found' message when search for undefined contexts", () => {
+    const notification = new Notification();
+
+    expect(notification.messages("asd")).toBe("no errors found");
+  });
+
+  it("should return all errors message when context not provided", () => {
+    const notification = new Notification();
+
+    const error = {
+      message: "error message",
+      context: "customer",
+    };
+
+    notification.addError(error);
+
+    const error2 = {
+      message: "error message2",
+      context: "customer",
+    };
+
+    notification.addError(error2);
+
+    const error3 = {
+      message: "error message2",
+      context: "order",
+    };
+
+    notification.addError(error3);
+
+    expect(notification.messages()).toBe(
+      "customer: error message, error message2. order: error message2. "
+    );
+  });
+});
---
+++ src/domain/@shared/notification/notification.ts
@@ -0,0 +1,34 @@
+export type NotificationError = {
+  context: string;
+  message: string;
+};
+
+export class Notification {
+  private errors: { [context: string]: string[] } = {};
+
+  addError(error: NotificationError): void {
+    if (!this.errors[error.context]) {
+      this.errors[error.context] = [];
+    }
+
+    this.errors[error.context].push(error.message);
+  }
+
+  messages(context?: string): string {
+    if (!context) {
+      let response = "";
+
+      Object.keys(this.errors).map((key) => {
+        response += `${key}: ${this.messages(key)}. `;
+      });
+
+      return response;
+    }
+
+    if (!this.errors[context]) {
+      return "no errors found";
+    }
+
+    return this.errors[context].join(", ");
+  }
+}
